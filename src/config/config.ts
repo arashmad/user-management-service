d2c44fa5746050c19dbded9b629002a0756682e0
arashmad <arashmad.dev@gmail.com>
2024-07-04 18:00:08 +0200

    extended the schema

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,53 +1,10 @@
 import dotenv from 'dotenv';
 
+import { IApplicationConfiguration } from '../interface/connection.interface';
+
 dotenv.config();
 
-/**
- * Database connection configuration
- * @param {string} type Database type, only `postgres` is supported
- * @param {string} host Database host, default is localhost
- * @param {number} port Database port, default is 5432
- * @param {string} database Database name
- * @param {string} username Database root username for establishing the connection
- * @param {string} password Database root password for establishing the connection
- *
- */
-interface IDatabaseConfiguration {
-    type: 'postgres';
-    host: string;
-    port: number;
-    database: string;
-    username: string;
-    password: string;
-}
-
-/**
- * Application Configuration Interface
- * @param {string} host Express application host, default is localhost
- * @param {number} port Express application port, default is 3302
- */
-interface IServerConfiguration {
-    host: string;
-    port: number;
-}
-
-/**
- * Application Configuration Interface
- * @param {IDatabaseConfiguration} db Database connection configuration
- * @param {IServerConfiguration} server Express application server configuration
- * @param {string} scope Scope of the application could be one of `test`, `development` and `production`
- */
-interface IConfiguration {
-    db: IDatabaseConfiguration;
-    server: IServerConfiguration;
-    scope: string;
-}
-
-Object.keys(process.env).map((name) => {
-    if (name.indexOf('_TEST_') >= 0) console.log(name, ':', process.env[name]);
-});
-
-export const configurationFactory = (): IConfiguration => {
+export const configurationFactory = (): IApplicationConfiguration => {
     const scope = process.env.NODE_ENV || 'development';
     switch (scope) {
         case 'test':
@@ -64,18 +21,20 @@ export const configurationFactory = (): IConfiguration => {
             }
             return {
                 db: {
-                    type: 'postgres',
                     host: process.env.PG_TEST_HOST,
                     port: Number(process.env.PG_TEST_PORT),
                     database: process.env.PG_TEST_DB_NAME,
-                    username: process.env.PG_TEST_USER,
+                    user: process.env.PG_TEST_USER,
                     password: process.env.PG_TEST_PASSWORD
                 },
                 server: {
                     host: process.env.SERVER_TEST_HOSTNAME,
                     port: Number(process.env.SERVER_TEST_PORT)
                 },
-                scope
+                tokenConfig: {
+                    secret: 'hahahaha',
+                    expireIn: '1h'
+                }
             };
 
         case 'development':
@@ -92,18 +51,20 @@ export const configurationFactory = (): IConfiguration => {
                 throw Error('Missing configuration setting. Check .env file.');
             return {
                 db: {
-                    type: 'postgres',
                     host: process.env.PG_DEV_HOST,
                     port: Number(process.env.PG_DEV_PORT),
                     database: process.env.PG_DEV_DB_NAME,
-                    username: process.env.PG_DEV_USER,
+                    user: process.env.PG_DEV_USER,
                     password: process.env.PG_DEV_PASSWORD
                 },
                 server: {
                     host: process.env.SERVER_DEV_HOSTNAME,
                     port: Number(process.env.SERVER_DEV_PORT)
                 },
-                scope
+                tokenConfig: {
+                    secret: 'hahahaha',
+                    expireIn: '1h'
+                }
             };
 
         case 'production':
@@ -119,18 +80,20 @@ export const configurationFactory = (): IConfiguration => {
                 throw Error('Missing configuration setting. Check .env file.');
             return {
                 db: {
-                    type: 'postgres',
                     host: process.env.PG_PROD_HOST,
                     port: Number(process.env.PG_PROD_PORT),
                     database: process.env.PG_PROD_DB_NAME,
-                    username: process.env.PG_PROD_USER,
+                    user: process.env.PG_PROD_USER,
                     password: process.env.PG_PROD_PASSWORD
                 },
                 server: {
                     host: process.env.SERVER_PROD_HOSTNAME,
                     port: Number(process.env.SERVER_PROD_PORT)
                 },
-                scope
+                tokenConfig: {
+                    secret: 'hahahaha',
+                    expireIn: '1h'
+                }
             };
     }
 };
