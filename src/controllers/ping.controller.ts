991f778d52a34699113d64f30edae98b98d068b0
arashmad <arashmad.dev@gmail.com>
2024-07-04 18:04:37 +0200

    renamed file

--- src/controllers/ping.controller.ts
+++ src/controllers/ping.controller.ts
@@ -1,11 +1,11 @@
 import { Request, Response, NextFunction } from 'express';
 
-const healthCheck = (req: Request, res: Response, next: NextFunction) => {
+const pingController = (req: Request, res: Response, next: NextFunction) => {
     return res.status(200).json({
         message: 'pong'
     });
 };
 
 export default {
-    healthCheck
+    pingController
 };
