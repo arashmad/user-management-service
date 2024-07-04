b7051d32d468efb68efbc2159bf29bc3cc1553d2
arashmad <arashmad.dev@gmail.com>
2024-07-04 18:07:17 +0200

    deleted

--- src/routes/user.ts
+++
@@ -1,7 +0,0 @@
-import { Router } from 'express';
-
-import UserControllers from '../controllers/user';
-
-export default (router: Router) => {
-    router.post('/user', UserControllers.createUserController);
-};
