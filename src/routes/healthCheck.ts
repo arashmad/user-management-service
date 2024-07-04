903b7c6f1105699470fd38396e2fecaf5b8f36e1
arashmad <arashmad.dev@gmail.com>
2024-07-04 18:11:24 +0200

    renamed

--- src/routes/healthCheck.ts
+++
@@ -1,7 +0,0 @@
-import { Router } from 'express';
-
-import controller from '../controllers/healthCheck';
-
-export default (router: Router) => {
-    router.get('/ping', controller.healthCheck);
-};
