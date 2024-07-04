edd312fcb69a393b55f3b465aa41067ceabd0dda
arashmad <arashmad.dev@gmail.com>
2024-07-04 18:01:37 +0200

    repalce package with node-postgres

--- src/database/index.ts
+++ src/database/index.ts
@@ -1,22 +1,42 @@
-import { DataSource } from 'typeorm';
+import { Pool, PoolClient } from 'pg';
 
 import { configurationFactory } from '../config/config';
-
-// Importing Models
-import { User } from '../models/user.model';
-
 const { db } = configurationFactory();
 
-const dataSourceFactory = new DataSource({
-    type: db.type,
-    host: db.host,
-    port: db.port,
-    database: db.database,
-    username: db.username,
-    password: db.password,
-    entities: [User],
-    synchronize: true,
-    logging: false
-});
+/**
+ * Asynchronously returns `PoolClient` object for executing SQL queries.
+ * @returns {Promise<PoolClient>} PoolClient object in Promise format
+ * @example
+ * const connection = await ConnectDB();
+ * const result = await connection.query<IOutput>('SELECT ... ;');
+ */
+const ConnectDB = async (): Promise<PoolClient> => {
+    try {
+        const pool = new Pool(db);
+        return await pool.connect();
+    } catch (error) {
+        throw error;
+    }
+};
 
-export default dataSourceFactory;
+// /**
+//  * Asynchronously executes a query.
+//  * @param {PoolClient} connection Connection object
+//  * @param {string|IQuery} query Query
+//  * @returns {Promise<PoolClient>} PoolClient object in Promise format
+//  * @example
+//  * const conn = await ConnectDB();
+//  * const result = await RunQuery<IOutput>(conn, query)('SELECT ... ;');
+//  */
+// const RunQuery = async (connection: PoolClient, query: string | IQuery): Promise<QueryResult<any>> => {
+//     try {
+//         const result = await connection.query(query);
+//         connection.release();
+//         return result;
+//     } catch (error) {
+//         connection.release();
+//         throw error;
+//     }
+// };
+
+export { ConnectDB };
